70a21271309bbcc1bf45bd7663bc2ab0c726052d
yaroslav <zhurba_ys@mail.ru>
2021-10-13 22:41:49 +0300

    add url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='foopy21',
     version='0.1.0',
     description='Packege use numpy 1.16.0 that conflicts with pandas 1.3.3',
-#    url='https://github.com/shuds13/pyexample',
+    url='https://github.com/YaroslavZhurba/foopy21',
     author='Yaroslav Zhurba',
     author_email='shudson@anl.gov',
     license='BSD 2-clause',
