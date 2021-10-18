3763a84989563f7a645636370185ec245df5b5c1
yaroslav <zhurba_ys@mail.ru>
2021-10-18 22:43:26 +0300

    add iszero function. Numpy shoul be 1.16.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='foopy21',
-    version='0.1.0',
+    version='0.1.1',
     description='Packege use numpy 1.16.0 that conflicts with pandas 1.3.3',
     url='https://github.com/YaroslavZhurba/foopy21',
     author='Yaroslav Zhurba',
-    author_email='shudson@anl.gov',
+    author_email='zhurba_ys@mail.ru',
     license='BSD 2-clause',
     packages=['foopy21'],
-    install_requires=['numpy<=1.16.0']
+    install_requires=['numpy==1.16.0']
 )
